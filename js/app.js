ae641baa6f06726083db3603806ce03636c2df89
Nicholas Glover <jacksaintjack@gmail.com>
2016-01-21 17:00:57 -0600

    Added the physics for when objects collide

--- js/app.js
+++ js/app.js
@@ -39,7 +39,7 @@ Game.Play.prototype = {
   create: function(){
 
     // ** Code for the Table ** //
-
+    this.stage.backgroundColor = 0x001b07;
     //Placing the sprite postion
     this.table = this.add.sprite(400, 300, 'table');
 
@@ -52,6 +52,8 @@ Game.Play.prototype = {
     this.table.body.static = true;
     this.table.body.clearShape(); //Makes sure we don't see physics enabled rectangle
     this.table.body.loadPolygon('table', 'pool-table-physics-shape'); // loads physics data from Cache
+    //loading matierla for table
+    this.tableMaterial = this.physics.p2.createMaterial('tableMaterial', this.table.body);
 
     // ** Code for the Pockets in the Pool //
     //Adding Blank Sprite for pockets
@@ -70,56 +72,199 @@ Game.Play.prototype = {
     this.pockets.body.addCircle(32, 736, 528);
 
     //** Code for Balls **//
+
+    //Shadow for the Ball
+    this.shadows = this.add.group();
+
+    //cushion placed on top of shadow sprite so that the shadow will not bleed over on other sprites
+    this.add.sprite(0, 0, 'cushions');
+
     this.balls = this.add.physicsGroup(Phaser.Phsyics.P2JS);
     this.balls.enableBodyDebug = Game.showDebug;
-  },
 
-  togglePause: function() {},
+    //Defining materials for the balls
+    this.ballMaterial = this.physics.p2.createMaterial('ballMaterial');
 
-  toggleDebug: function() {},
+    //Row one of Balls on Rack (200 is the x cordinate)
+    var y = 241;
+    this.makeBall(200, Game.RED);
+    this.makeBall(200, y + 32, Game.YELLOW);
+    this.makeBall(200, y + 64, Game.YELLOW);
+    this.makeBall(200, y + 96, Game.RED);
+    this.makeBall(200, y + 128, Game.YELLOW)
 
-  // Using this method to generate our balls. x,y stand for the Position
-  // The color is the color of the balls
-  makeBall: function(x, y, color) {
-    var ball = this.balls.create(x, y, 'balls', color);
+    //Row two
+    y = 257;
+    this.makeBall(232, y, Game.YELLOW);
+    this.makeBall(232, y + 32, Game.RED);
+    this.makeBall(232, y + 64, Game.YELLOW);
+    this.makeBall(232, y + 96, Game.RED);
 
-    ball.body.setCircle(13); //Body is set to a circle with 13px
-    ball.body.fixedRotation = true; //Makes sure balls don't rotate
-    ball.body.setMaterial(this.ballMaterial);
-    //Starts slowing down the ball after an impact.
-    ball.body.damping = 0.40;
-    ball.body.angularDamping = 0.45;
-    // this is called when the ball goes into the pocket
-    ball.body.createBodyCallback(this.pockets, this.hitPocket, this)
+    //Row Three
+    y = 273;
 
-    //Creating a shadow for my ball
-    var shadow = this.shadows.create(x + 4, y + 4, 'balls', 4);
-    shadow.anchor.set(0.5);
+    this.makeBall(264, y, Game.RED);
+    this.makeBall(264, y + 32, Game.BLACK);
+    this.makeBall(264, y + 64, Game.YELLOW);
 
-    ball.shadow = shadow;
+    //Row Four
+    y = 289;
+    this.makeBall(296, y, Game.YELLOW);
+    this.makeBall(296, y + 32, Game.RED);
 
-    return ball;
-  },
+    //Row Five
+    this.makeBall(328, 305, Game.RED)
 
-  takeShot: function () {},
+    //Cue Ball
+    this.cueball = this.makeBall(576, 305, Game.WHITE);
 
-  hitPocket: function (ball,pocket){},
+    //VS Physics
+    var ballVsTableMaterial = this.physics.p2.createContactMaterial(
+      this.ballMaterial, this.tableMaterial);
 
-  resetCueBall: function() {},
+      ballVsTableMaterial.restitution = 0.6; //bouncinessfor ball on table hits
 
-  placeCueBall: function() {},
+      var ballVsBallMaterial = this.physics.p2.createContactMaterial(
+        this.ballMaterial, this.ballMaterial);
 
-  updateCue: function () {},
+        ballVsBallMaterial.restitution = 0.9; // bouciness for ball on ball hits
 
-  update: function (){},
+        //Cue
+        this.cue = this.add.sprite(0, 0, 'cue');
+        this.cue.anchor.y = 0.5; // making sure the cue will always perfects be around cue ball
 
-  updateSpeed: function () {},
+        //The power indicator fill for cue
+        this.fill = this.add.sprite(0, 0, 'fill');
+        this.fill.anchor.y = 0.5;
+        this.fillRect = new Phaser.Rectangle(0, 0, 332, 6);
+        this.fill.crop(this.fillRect); // Cropping how much you see of the fill bar
 
-  preRender: function () {},
+        ///Created a phaser line object to handle pull back of the cue
+        this.aimLine = new Phaser.Line(
+          this.cueball.x, this.cueball.y,
+          this.cueball.x, this.cueball.y);
+        )
+      },
 
-  positionShadow: function () {},
+      togglePause: function() {},
 
-  gameOver: function () {},
+      toggleDebug: function() {},
 
-  render: function() {}
-};
+      // Using this method to generate our balls. x,y stand for the Position
+      // The color is the color of the balls
+      makeBall: function(x, y, color) {
+        var ball = this.balls.create(x, y, 'balls', color);
+
+        ball.body.setCircle(13); //Body is set to a circle with 13px
+        ball.body.fixedRotation = true; //Makes sure balls don't rotate
+        ball.body.setMaterial(this.ballMaterial);
+        //Starts slowing down the ball after an impact.
+        ball.body.damping = 0.40;
+        ball.body.angularDamping = 0.45;
+        // this is called when the ball goes into the pocket
+        ball.body.createBodyCallback(this.pockets, this.hitPocket, this)
+
+        // Binds the shadow Sprite to the ball sprite
+        var shadow = this.shadows.create(x + 4, y + 4, 'balls', 4);
+        shadow.anchor.set(0.5);
+
+        ball.shadow = shadow;
+
+        return ball;
+      },
+
+      takeShot: function () {
+        // First check to see how fast the cue ball is moving
+        if (this.speed > this.allowShotSpeed)
+        {
+          return;
+        }
+        // Capping speed to a max of 112
+        var speed = (this.aimLine.length / 3);
+
+        if (speed > 112)
+        {
+          speed = 112;
+        }
+
+        this.updateCue();
+
+        //adjusting speed to work out angle to apply a impulse to cue ball.
+        var px = (Math.cos(this.aimLine.angle) * speed);
+        var py = (Math.sin(this.aimLine.angle) * speed);
+
+        this.cueball.body.applyImpulse([ px, py ]); //Impulse is a force added to a body over a short period of time
+
+        //Hides the cue after the shot
+        this.cue.visible = false;
+        this.fill.visible = false;
+      },
+
+      hitPocket: function (ball, pocket){
+        //Checking to see if the cue ball went into the pocket
+        if (ball.sprite === this.cueball)
+        {
+          this.resetCueBall();
+        }
+      },
+
+      //handling when a cue ball is sunk into a pocket
+      resetCueBall: function() {
+        this.cueball.body.setZerVelocity(); // Stopping the velocity of the cue ball
+
+        this.cueball.body.x = 16;
+        this.cueball.body.y = 16;
+      },
+
+      placeCueBall: function() {},
+
+      //Updating the position of the aimline
+      updateCue: function () {
+        this.aimLine.start.set(this.cueball.x, this.cueball.y);
+        this.aimLine.end.set(this.input.activePoint.x, this.input.activePointer.y);
+
+        //Align cue sprite
+        this.cue.position.copyFrom(this.aimLine.start);
+        this.cue.rotation = this.aimLine.angle;
+
+        //Adjust for how much of the fill strite is visible
+        this.fill.position.copyFrom(this.aimLine.start);
+        this.fill.rotation = this.aimLine.angle;
+
+        this.fillRect.Width = this.aimLine.length;
+        this.fill.updateCrop();
+      },
+
+      update: function (){},
+
+      //Determines the speed of the cue ball
+      updateSpeed: function () {
+        this.speed = Math.sqrt(this.cueball.body.velocity.x * this.cueball.body.velocity.x + this.cueball.body.velocity.y * this.cueball.body.velocity.y);
+
+        if (this.speed < this.allowShotSpeed)
+        {
+          if (!this.cue.visible)
+          {
+            this.cue.visible = true;
+            this.fill.visible = true;
+          }
+        }
+        else if (this.speed < 3.0)
+        {
+          this.cueball.body.setZeroVelocity();
+        }
+      },
+
+      preRender: function () {
+        this.balls.forEach(this.positionShadow, this);
+      },
+
+      positionShadow: function (ball) {
+        ball.shadow.x = ball.x + 4;
+        ball.shadow.y = ball.y + 4;
+      },
+
+      gameOver: function () {},
+
+      render: function() {}
+    };
